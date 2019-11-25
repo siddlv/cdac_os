5ae6e116f7f87d4cc36fa47de876aa2f604e728e
siddlv <dalvisiddhesh09@gail.com>
2019-11-25 15:52:54 +0530

    Added a program to write simple system calls to open, write and close a file with error handling

---
+++ system_call/syscall.c
@@ -0,0 +1,42 @@
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <errno.h>
+
+int main(int argc, char const *argv[])
+{
+	int fd, w_return_size, return_close;
+	const void *buf = "hello\n";
+
+	//opening the file, if it doesnt exists then creating it and givinf approriate permissions
+	fd = open("syscall.txt", O_CREAT | O_WRONLY, 
+		S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+
+	if (fd != -1)
+	{
+		// writing data buf to the file of size 6
+		w_return_size = write(fd, buf, 6);
+	}
+	else
+		// if open() throws an error
+		perror("Error ");
+
+	if (w_return_size == -1)
+	{	
+		//if write() throws an error
+		perror("Error ");
+	}
+
+	// closing the opened file
+	return_close = close(fd);
+
+	if (return_close == -1)
+	{	
+		//if close throws an error
+		perror("Error ");
+	}
+
+	return 0;
+}
