ae8364cd4b6b81384671a21001d948be5e8bc9a8
siddlv <dalvisiddhesh09@gail.com>
2019-11-25 16:53:50 +0530

    Added simple priority task selection program without time constraints

---
+++ priority_task/priority.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{	
+	int tasks;
+	int tasks_arr[8];
+	int priority_task = 0;
+
+	printf("Enter the 8 bit task byte as an integer in binary:\n");
+	scanf("%d", &tasks);
+
+	for (int i = 7; i >= 0; --i)
+	{
+		tasks_arr[i] = tasks % 10;
+		tasks /= 10;
+	}
+
+	for (int i = 7; i >= 0; --i)
+	{
+		if (tasks_arr[i] == 1)
+		{
+			priority_task = i;
+			break;
+		}
+	}
+
+	printf("The task to be scheduled is %d\n", priority_task);
+	return 0;
+}
