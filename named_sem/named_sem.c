207cab400d9d4dfc6f82fbc446e8fb366bba4322
siddlv <dalvisiddhesh09@gmail.com>
2019-12-05 07:36:34 +0530

    Added program to implement named semaphore

---
+++ named_sem/named_sem.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <fcntl.h>           /* For O_* constants */
+#include <sys/stat.h>        /* For mode constants */
+#include <semaphore.h>
+
+int main(int argc, char const *argv[])
+{
+	/* code */
+	return 0;
+}
